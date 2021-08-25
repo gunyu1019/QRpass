27ca539c9e6131c7796f0e0973f59882d77cf50c
gunyu1019 <gunyu1019@gmail.com>
2021-08-25 14:42:51 +0900

    80%..?

--- mobile/src/main/java/kr/yhs/checkin/MainActivity.kt
+++ mobile/src/main/java/kr/yhs/checkin/MainActivity.kt
@@ -1,10 +1,9 @@
 package kr.yhs.checkin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import android.webkit.*
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.navigation.NavigationView
 import kr.yhs.checkin.databinding.ActivityMainBinding
 
@@ -12,6 +11,17 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private var mBinding: ActivityMainBinding? = null
     private val binding get() = mBinding!!
     private val pm = PackageManager("checkIn", this@MainActivity)
+    private val naQRBase = naQRBase
+
+    private fun getCookies(data: String): Map<String, String> {
+        val datas = data.split(";")
+        lateinit var result: MutableMap<String, String>
+        for (i in datas) {
+            val dataConvert = data.split("=")
+            result[dataConvert[0]] = dataConvert[1]
+        }
+        return result
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,18 +57,27 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                     override fun onPageFinished(view: WebView?, url: String?) {
                         super.onPageFinished(view, url)
 
-                        if (url == "https://nid.naver.com/login/privacyQR" && nidNL) {
+                        if (url == naQRBase && nidNL) {
+                            getCookies(
+                                cookie.getCookie(naQRBase)
+                            )
                             pm.setString("NID_PQR", "")
                             pm.setString("NID_AUT", "")
                             pm.setString("NID_SES", "")
                         }
                     }
                 }
-
-                if (cookie.getCookie("https://nid.naver.com/login/privacyQR") == null)
-                    loadUrl("https://nid.naver.com/nidlogin.login?url=https://nid.naver.com/login/privacyQR")
+                val delicious = cookie.getCookie(naQRBase)
+                if (delicious == null && nidNL)
+                    loadUrl("https://nid.naver.com/nidlogin.login?url=${naQRBase}")
                 else
-                    loadUrl("https://nid.naver.com/login/privacyQR")
+                    if (delicious == null) {
+                        cookie.setCookie(
+                            naQRBase,
+                            "NID_PQR=${pm.getString("NID_PQR")};NID_AUT=${pm.getString("NID_AUT")};NID_SES=${pm.getString("NID_SES")};"
+                        )
+                    }
+                    loadUrl(naQRBase)
             }
         }
     }
