6ca53478f416a574b414c66d45f385a861cc674e
gunyu1019 <gunyu1019@gmail.com>
2021-09-15 15:28:39 +0900

    Why didn't worked send wearable

--- mobile/src/main/java/kr/yhs/checkin/MainActivity.kt
+++ mobile/src/main/java/kr/yhs/checkin/MainActivity.kt
@@ -209,6 +209,7 @@ class MainActivity : AppCompatActivity() {
             if (login) {
                 processLogin()
             } else {
+                inputKey()
                 processMain()
             }
         }
