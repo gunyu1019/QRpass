d6e6314d3b2886aa76ebe7211cfa8444d964338c
gunyu1019 <gunyu1019@gmail.com>
2021-09-15 15:05:20 +0900

    Success Remake (need button setting)

--- mobile/src/main/java/kr/yhs/checkin/MainActivity.kt
+++ mobile/src/main/java/kr/yhs/checkin/MainActivity.kt
@@ -1,19 +1,23 @@
 package kr.yhs.checkin
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.os.Looper
+import android.util.Base64
 import android.view.View
 import android.webkit.CookieManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isInvisible
 import com.google.android.gms.wearable.DataClient
 import com.google.android.gms.wearable.PutDataMapRequest
 import com.google.android.gms.wearable.PutDataRequest
 import com.google.android.gms.wearable.Wearable
 import kr.yhs.checkin.databinding.ActivityMainBinding
 import android.view.animation.TranslateAnimation
-
-
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import org.jsoup.Jsoup
+import kotlin.concurrent.timer
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +27,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var dataClient: DataClient
 
     private var login: Boolean = false
+    private lateinit var typeMode: String
+    private lateinit var privacyNumber: String
+
+    private val naverLink = "https://nid.naver.com/login/privacyQR"
 
     private fun getCookies(data: String): Map<String, String> {
         val datas = data.split(";")
@@ -51,9 +59,125 @@ class MainActivity : AppCompatActivity() {
         }
         return
     }
+
+    private fun loadImage(base64: String) {
+        val base64Image: String = base64.split(",")[1]
+        val decodedString = Base64.decode(base64Image, Base64.DEFAULT)
+        val bitmap: Bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
+        binding.mainQR.setImageBitmap(bitmap)
+    }
+
+    private fun processMain() {
+        if (typeMode == "na") {
+            val pqr = pm.getString("NID_PQR")
+            val aut = pm.getString("NID_AUT")
+            val ses = pm.getString("NID_SES")
+            Thread (
+                Runnable {
+                    val response = Jsoup.connect(naverLink)
+                        .header("Cookie", "NID_PQR=${pqr};NID_AUT=${aut};NID_SES=${ses};")
+                        .get()
+                    val html = response.body()
+                    if (html.select("div.qr_wrap").html() != "") {
+                        val wrap = html.select("div.qr_wrap")
+                        if (wrap.select("div.qr_area").html() != "") {
+                            val area = wrap.select("div.qr_area")
+                            val base64 = area.select("div.qr_box img").attr("src")
+                            val numberHTML = area.select("div.number_box span.number")
+                            privacyNumber = numberHTML.text()
+
+                            this@MainActivity.runOnUiThread(java.lang.Runnable {
+                                loadImage(base64.toString())
+                                binding.privateCode.text = privacyNumber
+                                binding.refreshBtn.visibility = View.GONE
+                                binding.timerCount.text = getString(R.string.count, 15)
+
+                                var second = 0
+                                timer(period = 1000, initialDelay = 1000) {
+                                    runOnUiThread {
+                                        binding.timerCount.text = getString(R.string.count, 15 - second)
+                                    }
+                                    second++
+                                    if (second == 15) {
+                                        runOnUiThread {
+                                            binding.refreshBtn.visibility = View.VISIBLE
+                                        }
+                                        cancel()
+                                    }
+                                }
+                                return@Runnable
+                            })
+                        } else if (wrap.select(".self_box").html() != "") {
+                            this@MainActivity.runOnUiThread(java.lang.Runnable {
+                                binding.refreshBtn.visibility = View.GONE
+                                processLogin(getString(R.string.need_authorize))
+                                return@Runnable
+                            })
+                        }
+                    } else if (html.select(".login_wrap").html() != "") {
+                        this@MainActivity.runOnUiThread(java.lang.Runnable {
+                            binding.refreshBtn.visibility = View.GONE
+                            processLogin(getString(R.string.login_expired))
+                            return@Runnable
+                        })
+                    }
+                }
+            ).start()
+        }
+    }
+
+    private fun processLogin(comment: String? = null) {
+        val cookie = CookieManager.getInstance()
+        slideUp(binding.webViewLayout)
+        if (comment == null) {
+            binding.warningMessage.visibility = View.INVISIBLE
+        } else {
+            binding.warningMessage.text = comment
+        }
+
+        binding.webViewFrame.apply {
+            settings.apply {
+                javaScriptEnabled = true
+                domStorageEnabled = true
+                allowContentAccess = true
+                allowFileAccess = true
+                setSupportMultipleWindows(true)
+            }
+            webViewClient = object : WebViewClient() {
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+
+                    when {
+                        url == naverLink -> {
+                            val data = getCookies(
+                                cookie.getCookie(naverLink)
+                            )
+
+                            pm.setString("NID_PQR", data["NID_PQR"] ?: "")
+                            pm.setString("NID_AUT", data["NID_AUT"] ?: "")
+                            pm.setString("NID_SES", data["NID_SES"] ?: "")
+                            inputKey()
+                            slideDown(binding.webViewLayout)
+                            processMain()
+                            return
+                        }
+                        url ?: "".indexOf("https://nid.naver.com/nidlogin.login") == 0 -> {
+                            login = false
+                        }
+                        url ?: "".indexOf("https://nid.naver.com/iasystem/mobile_pop.nhn") == 0 -> {
+                            login = false
+                        }
+                    }
+                }
+            }
+            webViewClient.apply {
+                loadUrl(naverLink)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val cookie = CookieManager.getInstance()
         pm = PackageManager("checkIn", this@MainActivity)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -61,10 +185,14 @@ class MainActivity : AppCompatActivity() {
         binding.webViewLayout.visibility = View.INVISIBLE
         if (supportActionBar != null)
             supportActionBar!!.hide()
-        var typeMode = pm.getString("checkMode")
-        if (typeMode == null || typeMode == "") {
+        typeMode = pm.getString("checkMode")?: ""
+        if (typeMode == "") {
             pm.setString("checkMode", "na")
             typeMode = "na"
+        }
+
+        binding.refreshBtn.setOnClickListener {
+            processMain()
         }
 
         dataClient = Wearable.WearableOptions.Builder().setLooper(Looper.getMainLooper()).build().let { options ->
@@ -79,12 +207,14 @@ class MainActivity : AppCompatActivity() {
                 login = true
 
             if (login) {
-                slideUp(binding.webViewLayout)
+                processLogin()
+            } else {
+                processMain()
             }
         }
     }
 
-    fun slideUp(view: View) {
+    private fun slideUp(view: View) {
         view.visibility = View.VISIBLE
         val animate = TranslateAnimation(
             0F,
@@ -97,7 +227,7 @@ class MainActivity : AppCompatActivity() {
         view.startAnimation(animate)
     }
 
-    fun slideDown(view: View) {
+    private fun slideDown(view: View) {
         val animate = TranslateAnimation(
             0F,
             0F,
