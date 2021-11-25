3f66216c3bbfe46fd104cb8c0310443d43040406
gunyu1019 <gunyu1019@gmail.com>
2021-11-25 18:36:15 +0900

    Wearable Coding

--- wear/src/main/java/kr/yhs/qrcheck/MainActivity.kt
+++ wear/src/main/java/kr/yhs/qrcheck/MainActivity.kt
@@ -2,16 +2,51 @@ package kr.yhs.qrcheck
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import kr.yhs.qrcheck.client.BaseClient
+import kr.yhs.qrcheck.client.NaverClient
 import kr.yhs.qrcheck.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
     private var mBinding: ActivityMainBinding? = null
     private val binding get() = mBinding!!
+    private lateinit var pm: PackageManager
+    private var typeClient: Int = 0
+
+    private lateinit var client: BaseClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.main.visibility = View.GONE
+        binding.progressLayout.visibility = View.VISIBLE
+        binding.warningLayout.visibility = View.GONE
+
+        pm = PackageManager("QRpass", this@MainActivity)
+        typeClient = pm.getInt("checkMode", default = -1)
+
+        if (typeClient == -1) {
+            binding.main.visibility = View.GONE
+            binding.progressLayout.visibility = View.GONE
+            binding.warningLayout.visibility = View.VISIBLE
+            binding.warningMessage.text = getString(R.string.phone)
+            return
+        } else if(typeClient == 0) {
+            val pqr = pm.getString("NID_PQR")
+            val aut = pm.getString("NID_AUT")
+            val ses = pm.getString("NID_SES")
+            client = NaverClient(this)
+        }
+
+        binding.main.visibility = View.GONE
+        binding.progressLayout.visibility = View.VISIBLE
+        binding.warningLayout.visibility = View.GONE
+        client.setResource(
+            binding.privateCodeText,
+            binding.qrCodeImage
+        )
     }
 }
---
+++ wear/src/main/java/kr/yhs/qrcheck/client/BaseClient.kt
@@ -0,0 +1,67 @@
+package kr.yhs.qrcheck.client
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
+import android.webkit.WebView
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kr.yhs.qrcheck.MainActivity
+import kotlin.coroutines.CoroutineContext
+
+abstract class BaseClient(open val activity: MainActivity): CoroutineScope {
+    private var privateKeyResource: TextView? = null
+    private var qrImageResource: ImageView? = null
+    var responseStatus: Boolean? = null
+    lateinit var responseReason: String
+
+    lateinit var privateKeyResponse: String
+    lateinit var qrImageResponse: Any
+
+    open val baseLink = ""
+
+    fun setResource(
+        privateKey: TextView? = null,
+        qrImage: ImageView? = null
+    ) {
+        privateKeyResource = privateKey
+        qrImageResource = qrImage
+    }
+
+    open fun <T> onLoad(vararg args: T) {
+        mJob = Job()
+    }
+
+    private fun loadImageWithBase64(resource: String = qrImageResponse as String) {
+        val base64Image: String = resource.split(",")[1]
+        val decodedString = Base64.decode(base64Image, Base64.DEFAULT)
+        val bitmap: Bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
+        if (qrImageResponse != resource)
+            qrImageResponse = resource
+
+        qrImageResource!!.setImageBitmap(bitmap)
+    }
+
+    fun updateResource() {
+        if (privateKeyResource != null)
+            privateKeyResource!!.text = privateKeyResponse
+
+        if (qrImageResource != null)
+            loadImageWithBase64(
+                qrImageResponse as String
+            )
+    }
+
+    fun failedResource() {
+    }
+
+    abstract fun checkBaseLink(url: String): Boolean
+    abstract fun getData()
+
+    private lateinit var mJob: Job
+    override val coroutineContext: CoroutineContext
+        get() = mJob + Dispatchers.Main
+}
