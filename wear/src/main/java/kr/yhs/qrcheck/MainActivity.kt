4d352ec9bb6b7435a4db0cb0295f5f16e7ee5aa0
gunyu1019 <gunyu1019@gmail.com>
2021-11-26 01:59:53 +0900

    Wearable Connection

--- wear/src/main/java/kr/yhs/qrcheck/MainActivity.kt
+++ wear/src/main/java/kr/yhs/qrcheck/MainActivity.kt
@@ -1,13 +1,25 @@
 package kr.yhs.qrcheck
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.wear.phone.interactions.PhoneTypeHelper
+import androidx.wear.remote.interactions.RemoteActivityHelper
+import androidx.wear.widget.ConfirmationOverlay
+import com.google.android.gms.wearable.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.guava.await
+import kotlinx.coroutines.tasks.await
 import kr.yhs.qrcheck.client.BaseClient
 import kr.yhs.qrcheck.client.NaverClient
 import kr.yhs.qrcheck.databinding.ActivityMainBinding
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : Activity() {
+class MainActivity : Activity(), CoroutineScope, CapabilityClient.OnCapabilityChangedListener,
+    DataClient.OnDataChangedListener {
     private var mBinding: ActivityMainBinding? = null
     private val binding get() = mBinding!!
     private lateinit var pm: PackageManager
@@ -15,23 +27,53 @@ class MainActivity : Activity() {
 
     private lateinit var client: BaseClient
 
+    private lateinit var capabilityClient: CapabilityClient
+    private lateinit var nodeClient: NodeClient
+    private lateinit var remoteActivityHelper: RemoteActivityHelper
+
+    private var phoneNode: Node? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        mJob = Job()
+
+        capabilityClient = Wearable.getCapabilityClient(this)
+        nodeClient = Wearable.getNodeClient(this)
+        remoteActivityHelper = RemoteActivityHelper(this)
 
         binding.main.visibility = View.GONE
         binding.progressLayout.visibility = View.VISIBLE
         binding.warningLayout.visibility = View.GONE
 
         pm = PackageManager("QRpass", this@MainActivity)
-        typeClient = pm.getInt("checkMode", default = -1)
+        typeClient = pm.getInt("typeClient", default = -1)
+
+        binding.warningButton.setOnClickListener {
+            openAppProcess()
+        }
 
         if (typeClient == -1) {
+            when (PhoneTypeHelper.getPhoneDeviceType(applicationContext)) {
+                PhoneTypeHelper.DEVICE_TYPE_ANDROID -> {
+                    Log.d(TAG, "\tDEVICE_TYPE_ANDROID")
+                }
+                PhoneTypeHelper.DEVICE_TYPE_IOS -> {
+                    Log.d(TAG, "\tDEVICE_TYPE_IOS")
+                    binding.warningMessage.text = getString(R.string.ios_not_support)
+                    binding.warningButton.visibility = View.GONE
+                    return
+                }
+                else -> {
+                    Log.d(TAG, "\tDEVICE_TYPE_ERROR_UNKNOWN")
+                }
+            }
             binding.main.visibility = View.GONE
             binding.progressLayout.visibility = View.GONE
             binding.warningLayout.visibility = View.VISIBLE
+            binding.warningButton.visibility = View.VISIBLE
             binding.warningMessage.text = getString(R.string.phone)
             return
         } else if(typeClient == 0) {
@@ -39,6 +81,7 @@ class MainActivity : Activity() {
             val aut = pm.getString("NID_AUT")
             val ses = pm.getString("NID_SES")
             client = NaverClient(this)
+            client.onLoad(pqr, aut, ses)
         }
 
         binding.main.visibility = View.GONE
@@ -48,5 +91,135 @@ class MainActivity : Activity() {
             binding.privateCodeText,
             binding.qrCodeImage
         )
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        Wearable.getCapabilityClient(this).removeListener(this, CAPABILITY_PHONE_APP)
+        Wearable.getDataClient(this).addListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Wearable.getCapabilityClient(this).addListener(this, CAPABILITY_PHONE_APP)
+        Wearable.getDataClient(this).removeListener(this)
+        launch {
+            connectionProcess()
+        }
+    }
+
+    private fun connectionProcess() {
+        launch {
+            val capabilityInfo = capabilityClient
+                .getCapability(CAPABILITY_PHONE_APP, CapabilityClient.FILTER_ALL)
+                .await()
+            Log.d(TAG, "Capability request succeeded.")
+
+            withContext(Dispatchers.Main) {
+                phoneNode = capabilityInfo.nodes.firstOrNull()
+            }
+        }
+    }
+
+    private fun openAppProcess() {
+        Log.d(TAG, "openAppInStoreOnPhone()")
+
+        val intent = when (PhoneTypeHelper.getPhoneDeviceType(applicationContext)) {
+            PhoneTypeHelper.DEVICE_TYPE_ANDROID -> {
+                Log.d(TAG, "\tDEVICE_TYPE_ANDROID")
+                // Create Remote Intent to open Play Store listing of app on remote device.
+                Intent(Intent.ACTION_VIEW)
+                    .addCategory(Intent.CATEGORY_BROWSABLE)
+                    .setData(Uri.parse(ANDROID_MARKET_APP_URI))
+            }
+            PhoneTypeHelper.DEVICE_TYPE_IOS -> {
+                Log.d(TAG, "\tDEVICE_TYPE_IOS")
+
+                ConfirmationOverlay()
+                    .setType(ConfirmationOverlay.FAILURE_ANIMATION)
+                    .showOn(this@MainActivity)
+                return
+            }
+            else -> {
+                Log.d(TAG, "\tDEVICE_TYPE_ERROR_UNKNOWN")
+                return
+            }
+        }
+
+        launch {
+            try {
+                remoteActivityHelper.startRemoteActivity(intent).await()
+
+                ConfirmationOverlay().showOn(this@MainActivity)
+            } catch (cancellationException: CancellationException) {
+                // Request was cancelled normally
+                throw cancellationException
+            } catch (throwable: Throwable) {
+                ConfirmationOverlay()
+                    .setType(ConfirmationOverlay.FAILURE_ANIMATION)
+                    .showOn(this@MainActivity)
+            }
+        }
+    }
+
+    companion object {
+        const val CAPABILITY_PHONE_APP = "qrpass_phone_app"
+        private const val TAG = "MainActivity"
+        private const val ANDROID_MARKET_APP_URI =
+            "market://details?id=kr.yhs.qrpass"
+    }
+
+    private lateinit var mJob: Job
+    override val coroutineContext: CoroutineContext
+        get() = mJob + Dispatchers.Main
+
+    override fun onCapabilityChanged(capabilityInfo: CapabilityInfo) {
+        Log.d(TAG, "onCapabilityChanged(): $capabilityInfo")
+        phoneNode = capabilityInfo.nodes.firstOrNull()
+    }
+
+    override fun onDataChanged(data: DataEventBuffer) {
+        data.forEach{ event ->
+            Log.i(TAG, "onDataChanged(): data-received ${event.type}")
+            if (event.type == DataEvent.TYPE_CHANGED) {
+                event.dataItem.also { item ->
+                    Log.i(TAG, "onDataChanged(): item-url ${item.uri.path}")
+                    if (item.uri.path == "/naver/token") {
+                        DataMapItem.fromDataItem(item).dataMap.apply {
+                            val pqr = getString("kr.yhs.qrpass.token.NID_PQR") ?: ""
+                            val aut = getString("kr.yhs.qrpass.token.NID_AUT") ?: ""
+                            val ses = getString("kr.yhs.qrpass.token.NID_SES") ?: ""
+
+                            typeClient = 0
+                            pm.setInt("typeClient", typeClient)
+                            pm.setString("NID_PQR", pqr)
+                            pm.setString("NID_AUT", aut)
+                            pm.setString("NID_SES", ses)
+
+                            // webMain(
+                            //     mapOf(
+                            //         "NID_PQR" to pqr,
+                            //         "NID_AUT" to aut,
+                            //         "NID_SES" to ses
+                            //     )
+                            // )
+                            binding.main.visibility = View.GONE
+                            binding.progressLayout.visibility = View.VISIBLE
+                            binding.warningLayout.visibility = View.GONE
+                        }
+                    }
+                }
+            } else if (event.type == DataEvent.TYPE_DELETED) {
+                event.dataItem.also { item ->
+                    Log.i(TAG, "onDataChanged(): item-url ${item.uri.path}")
+                    if (item.uri.path == "/naver/token") {
+                        pm.removeKey("NID_PQR")
+                        pm.removeKey("NID_AUT")
+                        pm.removeKey("NID_SES")
+                    }
+                }
+            }
+        }
     }
 }
