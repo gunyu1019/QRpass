d082b48cc5c1f97afa7aa4fe0a732e9f09c5118d
gunyu1019 <gunyu1019@gmail.com>
2021-12-19 02:26:34 +0900

    progressBar!

--- wear/src/main/java/kr/yhs/qrcheck/activity/QRImageActivity.kt
+++ wear/src/main/java/kr/yhs/qrcheck/activity/QRImageActivity.kt
@@ -1,16 +1,18 @@
 package kr.yhs.qrcheck.activity
 
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.ProgressBar
 import kr.yhs.qrcheck.R
 import kotlin.concurrent.timer
 
 class QRImageActivity: MainResourceActivity() {
+    private lateinit var processBar: ProgressBar
+
     override fun onCreate() {
         imageView = view.findViewById(R.id.qrCodeImageView)
         refreshButton = view.findViewById(R.id.refreshBtn)
         timerTextView = view.findViewById(R.id.timerTextView)
+        processBar = view.findViewById(R.id.circularProgressIndicator)
 
         refreshButton.setOnClickListener {
             timerProcess()
@@ -19,20 +21,23 @@ class QRImageActivity: MainResourceActivity() {
         timerProcess()
     }
 
-    fun timerProcess() {
+    private fun timerProcess() {
         var second = 0
         context.client.getData()
         refreshButton.visibility = View.GONE
         timerTextView.text = context.getString(R.string.count, 15)
+        processBar.progress = 150
         timer(period = 1000, initialDelay = 1000) {
             context.runOnUiThread {
                 timerTextView.text = context.getString(R.string.count, 15 - second)
+                processBar.progress = (15 - second) * 10
             }
             second++
             if (second == 15) {
                 context.runOnUiThread {
                     timerTextView.text = context.getString(R.string.count, 0)
                     refreshButton.visibility = View.VISIBLE
+                    processBar.progress = 0
                 }
                 cancel()
             }
